bed818920ec3b0da27233f6b7feeadc937a67c72
ionisingTuna <bman3210@gmail.com>
2019-05-08 22:35:05 +0100

    First Build

---
+++ SourceBeam/app/components/Home.vue
@@ -0,0 +1,56 @@
+<template>
+    <Page class="page">
+        <ActionBar class="action-bar">
+            <Label class="action-bar-title" text="Home"></Label>
+        </ActionBar>
+
+        <StackLayout verticalAlignment="center">
+            <Button horizontalAlignment="center" text="Toggle" @tap="set" /></Button>
+        </StackLayout>
+    </Page>
+</template>
+
+<script>
+  import * as flashlight from "nativescript-flashlight";
+    export default {
+        computed: {
+            message() {
+                return "Toggle.";
+            }
+        },
+        data () {
+          return {
+          toggle: true
+          }
+        },
+        methods: {
+          set: function () {
+              
+              flashlight.toggle({
+              intensity: 0.25
+            });
+            /*var brightness = new nativescriptBrightness.Brightness();
+            const brightness = new Brightness();
+              brightness.set({
+             intensity: 25
+          });
+          */
+          }
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    // Start custom common variables
+    @import '../app-variables';
+    // End custom common variables
+
+    // Custom styles
+    .fa {
+        color: $accent-dark;
+    }
+
+    .info {
+        font-size: 20;
+    }
+</style>
